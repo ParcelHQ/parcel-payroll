b8917219c0d1bcfcff7b5009cadec82f29637497
Krishna Sharma <rohansharma.8574@gmail.com>
2023-02-20 10:54:17 +0530

    Fixed .transfer to .call #22

--- contracts/payroll/PayrollManager.sol
+++ contracts/payroll/PayrollManager.sol
@@ -174,7 +174,7 @@ contract PayrollManager is Storage, Signature, ReentrancyGuard {
                 // Transfer the funds to the recipient (to) addresses
                 if (tokenAddress[i] == address(0)) {
                     // Transfer ether
-                    payable(to[i]).transfer(amount[i]);
+                    payable(to[i]).call{value: amount[i]}("");
                     packPayoutNonce(true, payoutNonce[i]);
                 } else {
                     // Transfer ERC20 tokens
