a2a796406a246af4e68a13ea54d7bad08bde1bae
Krishna Sharma <rohansharma.8574@gmail.com>
2023-03-07 12:01:57 +0400

    Added Remaining Fixes

--- contracts/PayrollFactory.sol
+++ contracts/PayrollFactory.sol
@@ -2,15 +2,18 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 interface OrganizerInterface {
     function initialize(
         address[] calldata _approvers,
         uint128 approvalsRequired
     ) external;
+
+    function transferOwnership(address newOwner) external;
 }
 
-contract ParcelPayrollFactory {
+contract ParcelPayrollFactory is Ownable2Step {
     address public immutable admin;
     address public immutable logic;
 
@@ -23,7 +26,7 @@ contract ParcelPayrollFactory {
         bytes initData
     );
 
-    constructor(address _logic) {
+    constructor(address _logic) Ownable2Step() {
         logic = _logic;
         admin = msg.sender;
     }
@@ -32,15 +35,20 @@ contract ParcelPayrollFactory {
         address[] calldata _approvers,
         uint128 approvalsRequired
     ) public returns (address) {
+        require(getParcelAddress[msg.sender] == address(0), "CS020");
+
         bytes memory _data = abi.encodeCall(
             OrganizerInterface.initialize,
             (_approvers, approvalsRequired)
         );
+
         TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
             logic,
-            tx.origin,
+            msg.sender,
             _data
         );
+
+        OrganizerInterface(address(proxy)).transferOwnership(msg.sender);
 
         getParcelAddress[msg.sender] = address(proxy);
         emit OrgOnboarded(msg.sender, address(proxy), logic, _data);
