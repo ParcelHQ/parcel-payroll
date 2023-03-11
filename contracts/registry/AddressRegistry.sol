3ffdd0f7985b09af00070b17086cf007659b998b
Krishna Sharma <rohansharma.8574@gmail.com>
2023-03-11 15:45:21 +0400

    Added fix for Pashov Comment

--- contracts/registry/AddressRegistry.sol
+++ contracts/registry/AddressRegistry.sol
@@ -10,37 +10,22 @@ interface IAddressRegistry {
 }
 
 contract AddressRegistry is Ownable2Step {
-    address internal constant SENTINEL_IMPLEMENTATION = address(0x1);
-    mapping(address => address) internal parcelWhitelistedImplementation;
-    uint256 internal parcelWhitelistedImplementationCount;
+    mapping(address => bool) internal parcelWhitelistedImplementation;
 
     constructor() Ownable2Step() {}
 
     function addNewImplementation(address _implementation) external onlyOwner {
         require(
             _implementation != address(0) &&
-                _implementation != SENTINEL_IMPLEMENTATION &&
                 _implementation != address(this) &&
                 _implementation != owner(),
             "CS001"
         );
-        // No duplicate implementation allowed.
-        require(
-            parcelWhitelistedImplementation[_implementation] == address(0),
-            "CS001"
-        );
-        parcelWhitelistedImplementation[
-            _implementation
-        ] = parcelWhitelistedImplementation[SENTINEL_IMPLEMENTATION];
-        parcelWhitelistedImplementation[
-            SENTINEL_IMPLEMENTATION
-        ] = _implementation;
-        parcelWhitelistedImplementationCount++;
+
+        parcelWhitelistedImplementation[_implementation] = true;
     }
 
     function isWhitelisted(address _implementation) public view returns (bool) {
-        return
-            _implementation != SENTINEL_IMPLEMENTATION &&
-            parcelWhitelistedImplementation[_implementation] != address(0);
+        return parcelWhitelistedImplementation[_implementation];
     }
 }
