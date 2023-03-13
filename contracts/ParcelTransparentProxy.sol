7553022e0b798fd15427e3f5b4b563779905c4e1
Krishna Sharma <rohansharma.8574@gmail.com>
2023-03-13 13:28:25 +0400

    Fixed Proxy version

--- contracts/ParcelTransparentProxy.sol
+++ contracts/ParcelTransparentProxy.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (proxy/transparent/TransparentUpgradeableProxy.sol)
-
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import "./registry/AddressRegistry.sol";
