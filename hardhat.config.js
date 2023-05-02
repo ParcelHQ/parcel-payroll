bf8815dcd769c551614c55169ada352a92e351a8
Sriram Kasyap Meduri <sriramkasyap.m.s@gmail.com>
2023-05-02 19:57:40 +0530

    Updated config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,15 +9,14 @@ module.exports = {
 
     etherscan: {
         apiKey: {
-            rinkeby: "C65HXF26NWE1WBFY1HURCT4JS8ZI7A4ZZE",
-            goerli: "WEQM48QVPQUPMV1EG2UKDCQJ4S52IYEB5E",
+            goerli: process.env.ETHERSCAN_API_KEY,
         },
     },
 
     networks: {
         hardhat: {
             forking: {
-                url: "https://eth-goerli.g.alchemy.com/v2/MT0pDHBrQ7J8fDTEPdPXcWD-JyAQbH0w",
+                url: process.env.TENDERLY_FORKING_HARDHAT,
             },
             allowUnlimitedContractSize: true,
         },
@@ -28,20 +27,6 @@ module.exports = {
                 : [],
         },
     },
-
-    // networks: {
-    //   mainnet: {
-    //     forking: {
-    //       url: "https://eth-mainnet.g.alchemy.com/v2/62V7EBvEFRA6DT_0RnOdq1c3ZH997ldQ",
-    //     },
-    //   },
-
-    //   goerli: {
-    //     forking: {
-    //       url: "https://eth-goerli.g.alchemy.com/v2/MT0pDHBrQ7J8fDTEPdPXcWD-JyAQbH0w",
-    //     },
-    //   },
-    // },
 
     solidity: {
         version: "0.8.17",
